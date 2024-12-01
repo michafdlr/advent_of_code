957d2c18ecd5fda03a08bec038d80cc8c272ba51
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-12-01 11:35:08 +0100

    day1 challenge two done

--- day1/main.py
+++ day1/main.py
@@ -1,7 +1,7 @@
-from utils import read_file
+from utils import read_file_to_lists, read_file_to_dicts
 
-def main():
-    list_1, list_2 = read_file("day1/input.txt")
+def ex1():
+    list_1, list_2 = read_file_to_lists("day1/input.txt")
     list_1.sort()
     list_2.sort()
     print(list_1[-10:], list_2[-10:])
@@ -10,6 +10,14 @@ def main():
         sum_of_distances += abs(list_1[i]-list_2[i])
     return sum_of_distances
 
+def ex2():
+    dict_1, dict_2 = read_file_to_dicts("day1/input.txt")
+    similarity_score = 0
+    for num in dict_1:
+        if num in dict_2:
+            similarity_score += int(num) * dict_1[num] * dict_2[num]
+    return similarity_score
 
 if __name__ == "__main__":
-    print(main())
+    print(ex1())
+    print(ex2())
--- day1/utils.py
+++ day1/utils.py
@@ -1,7 +1,6 @@
-def read_file(filename):
+def read_file_to_lists(filename):
     with open(filename, "r+") as f:
         lines = f.readlines()
-        print(lines[:10])
         list_1 = []
         list_2 = []
         for line in lines:
@@ -9,3 +8,14 @@ def read_file(filename):
             list_1.append(int(line[0]))
             list_2.append(int(line[-1]))
     return list_1, list_2
+
+def read_file_to_dicts(filename):
+    with open(filename, "r+") as f:
+        lines = f.readlines()
+        dict_1 = {}
+        dict_2 = {}
+        for line in lines:
+            line = line.rstrip().split()
+            dict_1[line[0]] = 1 if line[0] not in dict_1 else dict_1[line[0]] + 1
+            dict_2[line[1]] = 1 if line[1] not in dict_2 else dict_2[line[1]] + 1
+    return dict_1, dict_2
