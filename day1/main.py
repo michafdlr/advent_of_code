0f9a7325e5be99df492d498038e17dd1433b10c7
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-12-01 07:24:28 +0100

    day1 challenge one done

--- day1/main.py
+++ day1/main.py
@@ -1,8 +1,15 @@
 from utils import read_file
 
 def main():
-    lines = read_file("advent_of_code/day1/input.txt")
-    print(lines)
+    list_1, list_2 = read_file("day1/input.txt")
+    list_1.sort()
+    list_2.sort()
+    print(list_1[-10:], list_2[-10:])
+    sum_of_distances = 0
+    for i in range(len(list_1)):
+        sum_of_distances += abs(list_1[i]-list_2[i])
+    return sum_of_distances
+
 
 if __name__ == "__main__":
-    main()
+    print(main())
--- day1/utils.py
+++ day1/utils.py
@@ -1,4 +1,11 @@
 def read_file(filename):
-    with open(filename, "r") as f:
+    with open(filename, "r+") as f:
         lines = f.readlines()
-    return lines
+        print(lines[:10])
+        list_1 = []
+        list_2 = []
+        for line in lines:
+            line = line.rstrip().split()
+            list_1.append(int(line[0]))
+            list_2.append(int(line[-1]))
+    return list_1, list_2
