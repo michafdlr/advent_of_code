b8fbed00d59633e7aedb49bf863ee425bde9f36e
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-12-07 15:30:10 +0100

    part 2 day 7 done

--- day7/main.py
+++ day7/main.py
@@ -5,6 +5,12 @@ def check_value(values, test_value):
         return values[0] == test_value
     return (check_value([values[0] + values[1]] + values[2:], test_value) or check_value([values[0] * values[1]] + values[2:], test_value))
 
+def check_value_add(values, test_value):
+    if len(values) == 1:
+        return values[0] == test_value
+    return (check_value_add([values[0] + values[1]] + values[2:], test_value) or check_value_add([values[0] * values[1]] + values[2:], test_value)
+    or check_value_add([int(str(values[0]) + str(values[1]))] + values[2:], test_value))
+
 def ex1():
     test_values, values = read_file("day7/input.txt")
     calibration_result = 0
@@ -13,5 +19,14 @@ def ex1():
             calibration_result += test_values[i]
     return calibration_result
 
+
+def ex2():
+    test_values, values = read_file("day7/input.txt")
+    calibration_result = 0
+    for i in range(len(values)):
+        if check_value_add(values[i], test_values[i]):
+            calibration_result += test_values[i]
+    return calibration_result
+
 if __name__ == "__main__":
-    print(ex1())
+    print(ex2())
