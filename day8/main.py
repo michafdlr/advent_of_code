44600d9844d9cc76702461eaf96c3a973a6de98a
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-12-08 11:41:47 +0100

    part 2 day 8 done

--- day8/main.py
+++ day8/main.py
@@ -31,5 +31,35 @@ def ex1():
 
     return len(antinodes)
 
+def ex2():
+    grid = read_file("day8/input.txt")
+    rows, cols = len(grid), len(grid[0])
+
+    antennas = {}
+    antinodes = set()
+    for row in range(rows):
+        for col in range(cols):
+            el = grid[row][col]
+            if el != ".":
+                if el in antennas:
+                    for pos in antennas[el]:
+                        dx, dy = get_distance(pos, (row, col))
+                        antinode = (row + dx, col + dy)
+                        while check_bounds(antinode, rows, cols):
+                            antinodes.add(antinode)
+                            antinode = (antinode[0] + dx, antinode[1] + dy)
+                        antinode = (pos[0] - dx, pos[1] - dy)
+                        while check_bounds(antinode, rows, cols):
+                            antinodes.add(antinode)
+                            antinode = (antinode[0] - dx, antinode[1] - dy)
+                    antennas[el].append((row,col))
+                else:
+                    antennas[el] = [(row,col)]
+    antenna_positions = set()
+    for pos in antennas.values():
+        for el in pos:
+            antenna_positions.add(el)
+    return len(antinodes) + sum((len(antennas[pos]) for pos in antennas if len(antennas[pos])>1)) - len(antinodes & antenna_positions)
+
 if __name__ == "__main__":
-    print(ex1())
+    print(ex2())
