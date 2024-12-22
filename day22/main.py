a90da9eb68230cc2cd1b8fa9f9b2322b72fd96ba
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-12-22 14:02:25 +0100

    day 22 done

--- day22/main.py
+++ day22/main.py
@@ -5,6 +5,12 @@ def read_file(filename):
         numbers = list(map(int,f.read().splitlines()))
     return numbers
 
+# Note:
+# 64 = 2**6
+# 32 = 2**5
+# 2048 = 2**11
+# 16777216 = 2**24
+
 MODULO = 16777216
 
 def mix(old, new):
@@ -23,21 +29,74 @@ def step2(num):
 def step3(num):
     return prune(mix(num, num * 2048))
 
+"""
+def prune(value):
+    return value & 0x00FFFFFF
+
+def step1(num):
+    new = num << 6
+    return prune(num ^ new)
+
+def step2(num):
+    return prune(num ^ (num >> 5))
+
+def step3(num):
+    new = num << 11
+    return prune(num ^ new)
+"""
+
 def build_secret_number(num):
-    new_num = step1(num)
-    new_num = step2(new_num)
-    return step3(new_num)
+    return step3(step2(step1(num)))
 
 
 
 def ex1():
     numbers = read_file("day22/input.txt")
+    final_cache = {}
     new_numbers = []
     for num in numbers:
-        for _ in range(2000):
-            num = build_secret_number(num)
-        new_numbers.append(num)
+        if num not in final_cache:
+            temp = num
+            for _ in range(2000):
+                temp = build_secret_number(temp)
+            final_cache[num] = temp
+        new_numbers.append(final_cache[num])
     return sum(new_numbers)
+
+
+################### PART 2 ###################################
+
+def get_last_digit(num):
+    return int(str(num)[-1])
+
+
+def ex2():
+    numbers = read_file("day22/input.txt")
+    sequence_cache = {}
+    for num in numbers:
+        sequences = set()
+        digits = [get_last_digit(num)]
+        differences = []
+        for i in range(2000):
+            num = build_secret_number(num)
+            digit = get_last_digit(num)
+            if i == 0:
+                digits.append(digit)
+                differences.append(digits[1] - digits[0])
+            else:
+                digits[0], digits[1] = digits[1], digit
+                differences.append(digits[1] - digits[0])
+            if len(differences) == 4:
+                seq = tuple(differences)
+                if seq not in sequences:
+                    sequences.add(seq)
+                    if seq not in sequence_cache:
+                        sequence_cache[seq] = digit
+                    else:
+                        sequence_cache[seq] += digit
+                differences = differences[1:]
+    return max(sequence_cache.values())
 
 if __name__ == "__main__":
     print(ex1())
+    print(ex2())
