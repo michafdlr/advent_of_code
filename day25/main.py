99f002a446febe7bce983fbd2330ae5df1c12f30
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-12-25 07:09:25 +0100

    day 25 done -- to be optimized timewise

---
+++ day25/main.py
@@ -0,0 +1,50 @@
+import time
+from typing import NamedTuple
+
+def read_file(filename):
+    with open(filename, "r") as f:
+        parts = f.read().rstrip("\n").split("\n\n")
+    for i, part in enumerate(parts):
+        parts[i] = list(part.splitlines())
+    locks, keys = [], []
+    for part in parts:
+        if all(part[0][i] == "#" for i in range(len(part[0]))):
+            locks.append(part)
+        else:
+            keys.append(part)
+    return locks, keys
+
+
+def timer(func):
+    def wrapper():
+        start = time.time()
+        result = func()
+        end = time.time()
+        print(f"Time needed for function {func.__name__}: {end-start:.2f} sec\n")
+        return result
+    return wrapper
+
+def find_heights(element):
+    heights = [0 for _ in range(len(element[0]))]
+    for row in range(len(element)):
+        for col in range(len(element[0])):
+            if element[row][col] == "#":
+                heights[col] += 1
+    return tuple(heights)
+
+@timer
+def ex1():
+    locks, keys = read_file("day25/input.txt")
+    total = 0
+    for lock in locks:
+        l_heights = find_heights(lock)
+        for key in keys:
+            k_heights = find_heights(key)
+            if all(l_heights[i] + k_heights[i] <= 7  for i in range(len(l_heights))):
+                total += 1
+    return total
+
+
+
+if __name__ == "__main__":
+    print(f"Solution 1: {ex1()}\n", end=f"{20*'-'}\n")
